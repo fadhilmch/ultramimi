c77d6fcbff4c7036c37f58b710e64000386f11a2
Aditya Rachman <adityarputra@gmail.com>
2017-06-19 16:35:24 +0700

    Add Prefab and script

--- Assets/RandomizeDrop.cs
+++ Assets/RandomizeDrop.cs
@@ -5,32 +5,46 @@ using UnityEngine;
 public class RandomizeDrop : MonoBehaviour {
 	public int milkCount = 20;
 	public int obstacleCount = 10;
-	public int counter = 60;
+	public int counterP1 = 60;
+	public int counterP2 = 60;
+	public float spawnTime = 1.6f;
+	public GameObject milk;
 
-	private Vector2[] spawnCoordinateP2;
-	private Vector2[] spawnCoordinateP1;
-
+	private Vector3[] spawnCoordinateP2;
+	private Vector3[] spawnCoordinateP1;
+	private Transform[] spawnPoints;
 	// Use this for initialization
 	void Start () {
-		spawnCoordinateP2 = new Vector2[]
+		spawnCoordinateP2 = new Vector3[]
 		{
-			new Vector2(6.93, 0),
-			new Vector2(5.36,0),
-			new Vector2(3.55,0)
+			new Vector3(6.93f, 5.73f,0),
+			new Vector3(5.36f,5.73f,0),
+			new Vector3(3.55f,5.73f,0)
 		};	
-
-		spawnCoordinateP1 = new Vector2[]
+	
+		spawnCoordinateP1 = new Vector3[]
 		{
-			new Vector2(-3.25, 0),
-			new Vector2(-4.99,0),
-			new Vector2(-6.7,0)
+			new Vector3(-3.25f, 5.73f,0),
+			new Vector3(-4.99f,5.73f,0),
+			new Vector3(-6.7f,5.73f,0)
 		};
 
-
+		InvokeRepeating ("SpawnMilkP1", spawnTime + Random.Range(0,0.4f), spawnTime + Random.Range(0,0.2f));
+		InvokeRepeating ("SpawnMilkP2", spawnTime + Random.Range(0,0.4f), spawnTime + Random.Range(0,0.2f));
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void SpawnMilkP1 () {
+		if (counterP1 > 1) {
+			Instantiate (milk, spawnCoordinateP1 [Random.Range (0, 3)], new Quaternion ());
+			counterP1--;
+		}
+	}
+
+	void SpawnMilkP2 () {
+		if (counterP2 > 1) {
+			Instantiate (milk, spawnCoordinateP2 [Random.Range (0, 3)], new Quaternion ());
+			counterP2--;
+		}
 	}
 }
