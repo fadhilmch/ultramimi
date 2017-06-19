1f10c412c70fbfbdd13de728dc59f052fb143a45
Fadhil Mochammad <fadhillmch1095@gmail.com>
2017-06-19 11:29:43 +0700

    Some minor updates in game 2 scene

---
+++ Assets/Script/Games/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	
+	private PlayerController controller;
+	private Vector2 positionLeft;
+	private Vector2 positionMid;
+	private Vector2 positionRight;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<PlayerController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch(controller.moving){
+		case -1:
+			transform.position = new Vector2 (-1, 0);
+
+			break;
+		
+		case 0:
+			transform.position = new Vector2 (0, 0);
+			break;
+
+		case 1:
+			transform.position = new Vector2 (1, 0);
+			break;
+			
+			print (transform.position);
+		}
+	}
+}
