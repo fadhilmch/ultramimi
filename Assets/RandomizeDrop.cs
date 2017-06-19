20a368d4c29f86931e92bcd2de50c70fce8f2e99
Aditya Rachman <adityarputra@gmail.com>
2017-06-19 14:37:01 +0700

    Script some cs and stuff

---
+++ Assets/RandomizeDrop.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeDrop : MonoBehaviour {
+	public int milkCount = 20;
+	public int obstacleCount = 10;
+
+	private Vector2[] spawnCoordinateP2;
+	private Vector2[] spawnCoordinateP1;
+
+	// Use this for initialization
+	void Start () {
+		spawnCoordinateP2 = new Vector2[]
+		{
+			new Vector2(-4.48, 0),
+			new Vector2(-6.2,0),
+			new Vector2(-7.92,0)
+		};	
+
+		spawnCoordinateP2 = new Vector2[]
+		{
+			new Vector2(-13.55, 0),
+			new Vector2(-15.2,0),
+			new Vector2(-16.92,0)
+		};
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
