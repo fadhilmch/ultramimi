911d8c2a6199b77247731a187a320baf4269b953
Aditya Rachman Putra <adityarputra@gmail.com>
2017-07-27 18:31:04 +0700

    ganti dikit lupa aapa

--- Assets/Script/Pabrik.cs
+++ Assets/Script/Pabrik.cs
@@ -41,7 +41,7 @@ public class Pabrik : MonoBehaviour
     }
     void ReadInput()
     {
-        if (Input.GetKey(interaction.keyCode))
+		if (Input.GetKey(interaction.keyCode) || SerialHandler.getSensorDown((int)interaction.sensorTrigger1))
         {
             if (animator.GetCurrentAnimatorStateInfo(0).IsName("PABRIK_idle"))
             {
@@ -66,7 +66,7 @@ public class Pabrik : MonoBehaviour
             }
         }
 
-     
+     	/*
         else if (SerialHandler.serial_is_open && SerialHandler.getSensorDown((int)interaction.sensorTrigger1))
         {
             if (animator.GetCurrentAnimatorStateInfo(0).IsName("PABRIK_idle"))
@@ -92,7 +92,7 @@ public class Pabrik : MonoBehaviour
             }
 
         }
-
+		*/
         /*
         else if (Input.GetKey(interaction.keyCode) == false && (SerialHandler.serial_is_open && SerialHandler.getSensorDown((int)interaction.sensorTrigger1) == false))
         {
